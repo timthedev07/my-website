258f6682dd2df0891348a47e30a4a7900d576f4a
Tim <harvard90873@gmail.com>
2021-08-26 21:21:35 +0200

    complete post data fetching

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,1 +1,47 @@
-export const hi = "";
+import type { NextPage } from "next";
+import { GetStaticProps, GetStaticPaths } from "next";
+import { readdirSync, readFileSync } from "fs";
+import { join } from "path";
+
+interface Props {
+  slug: string;
+  content: string;
+}
+
+const Slug: NextPage<Props> = ({ slug, content }) => {
+  return (
+    <>
+      <div>Slug {slug}:</div>
+      <pre>{content}</pre>
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const fileNames = readdirSync("posts");
+
+  const paths = fileNames.map((fileName) => ({
+    params: {
+      slug: fileName.replace(".md", ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params as any as Props;
+  const content = readFileSync(join("posts", slug + ".md")).toString();
+
+  return {
+    props: {
+      slug,
+      content,
+    },
+  };
+};
+
+export default Slug;
