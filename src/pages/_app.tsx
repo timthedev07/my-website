6e3a5ea0cc11e7aaa3b688b5306b36bd1cc3fb7f
Tim <harvard90873@gmail.com>
2021-08-26 20:24:12 +0200

    import tailwind css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
+import "tailwindcss/tailwind.css";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
