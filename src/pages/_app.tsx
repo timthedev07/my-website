6b0635e040a29ace8374b53aaa4da031f96be833
Tim <harvard90873@gmail.com>
2021-08-26 20:12:29 +0200

    wrap in layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
 import type { AppProps } from "next/app";
+import { Layout } from "../components/layout";
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 };
 export default App;
