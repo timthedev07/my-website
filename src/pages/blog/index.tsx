19ef98e6f7e846aae7567d9d48584525cc9fad42
Tim <harvard90873@gmail.com>
2021-08-26 20:26:40 +0200

    rename component

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from "next";
 
-const Home: NextPage = () => {
+const Blogs: NextPage = () => {
   return <div>Posts:</div>;
 };
 
-export default Home;
+export default Blogs;
