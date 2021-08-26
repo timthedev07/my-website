e7ca7224b74ed148f6a48e06e8d6a8cf891bf32f
Tim <harvard90873@gmail.com>
2021-08-26 20:15:58 +0200

    complete navigation component toggle logic

--- src/components/nav/Entry.tsx
+++ src/components/nav/Entry.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Navbar } from "./Navbar";
+import { Sidebar } from "./Sidebar";
 
 interface EntryProps {}
 
 const BREAK_POINT = 300;
 
 export const NavigationEntry: React.FC<EntryProps> = ({}) => {
-  return <div></div>;
+  const [windowWidth, setWindowWidth] = useState<number>(window.innerWidth);
+
+  useEffect(() => {
+    const resizeHandler = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", resizeHandler);
+
+    return () => {
+      window.removeEventListener("resize", resizeHandler);
+    };
+  });
+
+  return windowWidth > BREAK_POINT ? <Navbar /> : <Sidebar />;
 };
