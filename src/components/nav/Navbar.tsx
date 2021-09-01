22212522229aac93636cbd380a5c3c23b4500af2
Tim <harvard90873@gmail.com>
2021-09-01 19:51:18 +0200

    more links

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -21,11 +21,25 @@ const NAV_LINKS: NavbarItemProps[] = [
     href: "/contact",
     name: "contact",
   },
+  {
+    href: "/about",
+    name: "about",
+  },
+  {
+    href: "/projects",
+    name: "projects",
+  },
+  {
+    href: "/blog",
+    name: "blog",
+  },
 ];
 
 export const Navbar: FC = ({}) => {
   return (
     <div className="dark:bg-black h-10 flex justify-around items-center">
+      <img className="w-9 h-9" src="/images/timthedev07.svg" alt="" />
+
       {NAV_LINKS.map((navLink) => {
         return <NavbarItem {...navLink} />;
       })}
