b10ae54c7716b3ebf379a9ace07043ea6081e7b8
Tim <harvard90873@gmail.com>
2021-09-01 19:35:40 +0200

    update Navbar.tsx

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { FC } from "react";
+import Link from "next/link";
 
-interface NavbarProps {}
+interface NavbarItemProps {
+  name: string;
+  href: string;
+}
 
-export const Navbar: React.FC<NavbarProps> = ({}) => {
-  return <div></div>;
+const NavbarItem: FC<NavbarItemProps> = ({ name, href }) => {
+  return (
+    <Link href={href} passHref={true}>
+      <a className="uppercase text-gray-800 transition duration-300 ease-in-out dark:text-gray-50 dark:hover:text-gray-300">
+        {name}
+      </a>
+    </Link>
+  );
+};
+
+const NAV_LINKS: NavbarItemProps[] = [
+  {
+    href: "/contact",
+    name: "contact",
+  },
+];
+
+export const Navbar: FC = ({}) => {
+  return (
+    <div className="dark:bg-black h-10 flex justify-around items-center">
+      {NAV_LINKS.map((navLink) => {
+        return <NavbarItem {...navLink} />;
+      })}
+    </div>
+  );
 };
