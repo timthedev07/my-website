6edd9a6fe5de1aae0d1d29cf3857609a53267def
Tim <harvard90873@gmail.com>
2021-09-04 16:29:13 +0200

    add blank header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>MY WEBSITE</div>;
+  return (
+    <div>
+      <header className="h-screen">
+        <img className="w-full h-full" />
+      </header>
+    </div>
+  );
 };
 
 export default Home;
