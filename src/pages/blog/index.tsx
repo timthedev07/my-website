ec121ed020c2bd8e42bb3ef6e9f211597f6271f5
Tim <harvard90873@gmail.com>
2021-09-04 18:53:25 +0200

    get a list of blogs

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,7 +1,37 @@
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
+import { readdirSync } from "fs";
 
-const Blogs: NextPage = () => {
-  return <div>Posts:</div>;
+interface Props {
+  posts: string[];
+}
+
+const Blogs: NextPage<Props> = ({ posts }) => {
+  return (
+    <div>
+      Posts:
+      <ol>
+        {posts.map((each) => (
+          <li key={each}>
+            {each
+              .split("-")
+              .map((each) => each.charAt(0).toUpperCase() + each.slice(1))
+              .join(" ")}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 };
 
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = readdirSync("posts");
+
+  const paths = posts.map((each) => each.replace(".md", ""));
+
+  return {
+    props: {
+      posts: paths,
+    } as Props,
+  };
+};
 export default Blogs;
