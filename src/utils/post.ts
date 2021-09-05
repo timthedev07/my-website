3b1e53c6c25ad8057de91cb1b3a03da5ff225374
Tim <harvard90873@gmail.com>
2021-09-05 11:36:16 +0200

    add helper

---
+++ src/utils/post.ts
@@ -0,0 +1,11 @@
+import matter from "gray-matter";
+import { MarkdownMetadata } from "../types/posts";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+export const getPostMetadata = (filename: string): MarkdownMetadata => {
+  const fileContent = readFileSync(join("post", filename)).toString();
+  const { data } = matter(fileContent);
+
+  return data as MarkdownMetadata;
+};
