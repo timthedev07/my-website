0eddda3df59e4c6db112cafea9b9d96c6c5ef196
Tim <harvard90873@gmail.com>
2021-09-05 14:22:46 +0200

    set width

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,37 +1,54 @@
 import type { GetStaticProps, NextPage } from "next";
 import { readdirSync } from "fs";
 import Link from "next/link";
+import { getPostMetadata } from "../../utils/post";
+import { MarkdownMetadata } from "../../types/posts";
 
 interface Props {
-  fileNames: string[];
+  filenamesWithMetadata: { filename: string; metadata: string }[];
 }
 
-const Blogs: NextPage<Props> = ({ fileNames }) => {
+const Blogs: NextPage<Props> = ({ filenamesWithMetadata }) => {
   return (
-    <div>
-      <h3 className=" text-3xl ">Posts:</h3>
-      <ol>
-        {fileNames.map((each) => (
-          <Link href={`blog/${each}`}>
-            <li key={each} className="m-6">
-              {each
-                .split("-")
-                .map((each) => each.charAt(0).toUpperCase() + each.slice(1))
-                .join(" ")}
-            </li>
-          </Link>
-        ))}
+    <>
+      <h3 className="text-4xl mx-8">Posts:</h3>
+      <ol className="w-full p-8">
+        {filenamesWithMetadata.map(
+          ({ filename, metadata: metadataAsString }) => {
+            const metadata = JSON.parse(metadataAsString) as MarkdownMetadata;
+            return (
+              <Link key={filename} href={`blog/${filename}`}>
+                <li className="w-full h-28 p-3  bg-component-primary-bg shadow-lg rounded-xl my-6">
+                  <div className="text-xl">
+                    {filename
+                      .split("-")
+                      .map(
+                        (each) => each.charAt(0).toUpperCase() + each.slice(1)
+                      )
+                      .join(" ")}
+                  </div>
+                  <div>{new Date(metadata.date).toDateString()}</div>
+                </li>
+              </Link>
+            );
+          }
+        )}
       </ol>
-    </div>
+    </>
   );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const fileNames = readdirSync("posts").map((each) => each.replace(".md", ""));
+  const fileNames = readdirSync("posts");
+
+  const fileNamesWithMetadata = fileNames.map((fileName) => ({
+    filename: fileName.replace(".md", ""),
+    metadata: JSON.stringify(getPostMetadata(fileName)),
+  }));
 
   return {
     props: {
-      fileNames,
+      filenamesWithMetadata: fileNamesWithMetadata,
     } as Props,
   };
 };
