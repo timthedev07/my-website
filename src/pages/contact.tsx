d133b8db497aaaae3b8b2841567ba40253b2d83d
Tim <harvard90873@gmail.com>
2021-09-10 21:48:21 +0200

    initialize contact page

---
+++ src/pages/contact.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from "next";
+import Link from "next/link";
+
+const Contact: NextPage = () => {
+  return (
+    <div className="p-5 flex justify-around items-center">
+      <div>
+        <h1 className="text-5xl">Reach me at</h1>
+        <ul>
+          <Link href="mailto:timpersonal07@gmail.com">
+            <a>
+              <li>Email</li>
+            </a>
+          </Link>
+          <Link href="https://github.com/timthedev07">
+            <a>
+              <li>GitHub</li>
+            </a>
+          </Link>
+          <Link href="https://www.reddit.com/user/im-just-a-dev">
+            <a>
+              <li>Reddit</li>
+            </a>
+          </Link>
+        </ul>
+      </div>
+      <div>
+        <img className="animate-spin" src="/images/tech-ring.png" />
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
