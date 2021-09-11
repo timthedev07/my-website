6ad6e1509f419b31fda6208c241f67304f7c79d0
Tim <harvard90873@gmail.com>
2021-09-11 20:33:55 +0200

    map nav links

--- src/components/nav/BottomNav.tsx
+++ src/components/nav/BottomNav.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { NAV_LINKS } from "./Navbar";
+import Link from "next/link";
 
-interface BottomNavProps {}
-
-export const BottomNav: React.FC<BottomNavProps> = ({}) => {
-  return <div></div>;
+export const BottomNav: React.FC = ({}) => {
+  return (
+    <ul className="py-5 w-full flex justify-around items-center">
+      {NAV_LINKS.map(({ name, href }) => (
+        <Link key={name} href={href}>
+          <a>
+            <li>
+              <img
+                className="w-10 h-10"
+                src={`/images/bottomnav/${name}.svg`}
+                alt=""
+              />
+            </li>
+          </a>
+        </Link>
+      ))}
+    </ul>
+  );
 };
