dd4bc678f80b050e45e1732ae8fe5b1f5d707130
Tim <harvard90873@gmail.com>
2021-09-11 21:09:10 +0200

    nav color

--- src/components/nav/BottomNav.tsx
+++ src/components/nav/BottomNav.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 export const BottomNav: React.FC = ({}) => {
   return (
-    <ul className="py-5 w-full flex justify-around items-center">
+    <ul className="py-5 w-full flex justify-around items-center fixed bottom-0 bg-dark-bg">
       {NAV_LINKS.map(({ name, href }) => (
         <Link key={name} href={href}>
           <a>
             <li>
               <img
-                className="w-10 h-10"
+                className="w-8 h-8"
                 src={`/images/bottomnav/${name}.svg`}
                 alt=""
               />
