c15e5911663b4679d0c9fdfd47a0c9d0fc31d674
Tim <harvard90873@gmail.com>
2022-05-01 12:53:12 +0200

    fix layout

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -27,18 +27,23 @@ const Contact: NextPage = () => {
         title: "Contact Me",
       })}
       <div className="flex w-full justify-center items-center">
-        <div className="justify-evenly flex mt-20 items-center border border-slate-400/40 rounded-xl bg-slate-700/10 flex-col gap-4 px-4 pt-10 pb-14 sm:flex-row sm:w-[95%] sm:px-5 sm:py-6 md:py-10 md:px-0 md:gap-4 md:w-10/12">
-          <h3 className="">Reach me at</h3>
-          <section className="flex justify-around items-center">
-            <ContactLink href="mailto:timpersonal07@gmail.com" name="email" />
-            <ContactLink href="https://github.com/timthedev07" name="github" />
-            <ContactLink
-              href="https://www.reddit.com/user/im-just-a-dev"
-              name="reddit"
-            />
-          </section>
+        <div className="justify-evenly flex mt-20 items-center border border-slate-400/40 rounded-xl bg-slate-700/10 flex-col gap-4 px-4 pt-10 pb-14 w-[98%] sm:flex-row sm:w-[95%] sm:px-5 sm:py-6 md:py-10 md:px-0 md:gap-4 md:w-10/12">
+          <div>
+            <h3 className="">Reach me at</h3>
+            <section className="flex justify-around items-center">
+              <ContactLink href="mailto:timpersonal07@gmail.com" name="email" />
+              <ContactLink
+                href="https://github.com/timthedev07"
+                name="github"
+              />
+              <ContactLink
+                href="https://www.reddit.com/user/im-just-a-dev"
+                name="reddit"
+              />
+            </section>
+          </div>
+          <ContactForm className="w-96" />
         </div>
-        <ContactForm className="w-96" />
       </div>
     </>
   );
