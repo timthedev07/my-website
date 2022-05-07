4c8c750b860c47b46b0b79d4a6bba709b96f8fd2
Tim <harvard90873@gmail.com>
2022-05-07 14:05:46 +0200

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div>
-      <header className="h-screen w-screen flex justify-center items-center">
+      <header className="h-screen w-[95%] flex justify-center items-center">
         <h2>Home page coming soon...</h2>
       </header>
     </div>
