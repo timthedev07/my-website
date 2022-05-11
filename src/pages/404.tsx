16d4b9f551e343ed06c7a45f58ae83f876cbc68c
Harvard90873 <harvard90873@gmail.com>
2022-05-11 07:08:34 +0000

    improve unimplemented pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,14 @@
 import type { NextPage } from "next";
+import { useEffect } from "react";
+import { useNavContext } from "../components/nav/Navbar";
 
 const NotFound: NextPage = () => {
+  const {setNavTransparent} = useNavContext();
+
+  useEffect(() => {
+    setNavTransparent(false);
+  }, [])
+
   return <div>404</div>;
 };
 
