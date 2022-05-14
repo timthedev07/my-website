872485d58bbaec518f985fb0c0b1b524e1ff9bb1
Tim <harvard90873@gmail.com>
2022-05-14 22:22:19 +0200

    regenerate

--- env.d.ts
+++ env.d.ts
@@ -8,6 +8,8 @@ declare global {
       DB_COLLECTION_NAME: string;
       GOOGLE_CLIENT_ID: string;
       GOOGLE_CLIENT_SECRET: string;
+      SPOTIFY_CLIENT_ID: string;
+      SPOTIFY_CLIENT_SECRET: string;
     }
   }
 }
