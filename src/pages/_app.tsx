66cb4f963293cd2e1b80964914e34b93ec2c576b
Tim <harvard90873@gmail.com>
2022-05-14 21:49:55 +0200

    session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,18 @@
 import { DragontailProvider } from "dragontail-experimental";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
+import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
-    <DragontailProvider theme="dark">
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </DragontailProvider>
+    <SessionProvider session={session}>
+      <DragontailProvider theme="dark">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </DragontailProvider>
+    </SessionProvider>
   );
 };
 export default App;
