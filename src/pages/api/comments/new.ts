0cc513cc93eacc9a9fde32d1e77157fc8d51c03d
Tim <harvard90873@gmail.com>
2022-05-15 16:10:46 +0200

    apply custom guards

--- src/pages/api/comments/new.ts
+++ src/pages/api/comments/new.ts
@@ -1,20 +1,10 @@
 import { NextApiHandler } from "next";
-import { getSession } from "next-auth/react";
 import { BlogFormData } from "../../../components/CommentForm";
+import { withAuth } from "../../../lib/middlewares/authRoute";
+import { withMethodGuard } from "../../../lib/middlewares/methodGuard";
 import { newComment } from "../../../mongodb/functions/newComment";
 
 const handler: NextApiHandler = async (req, res) => {
-  const session = await getSession({ req });
-
-  if (req.method?.toUpperCase() !== "POST") {
-    res.status(405).end();
-  }
-
-  if (!session?.user) {
-    res.status(401).end();
-    return;
-  }
-
   const formData: BlogFormData = JSON.parse(req.body);
 
   try {
@@ -25,4 +15,4 @@ const handler: NextApiHandler = async (req, res) => {
   }
 };
 
-export default handler;
+export default withMethodGuard(withAuth(handler), "POST");
