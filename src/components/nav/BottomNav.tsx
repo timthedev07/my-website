9791f436ec9de38723df5ea7f57430e1d399222a
Tim <harvard90873@gmail.com>
2022-05-15 16:34:05 +0200

    enhance svg usage in bottom nav

--- src/components/nav/BottomNav.tsx
+++ src/components/nav/BottomNav.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import { NAV_LINKS } from "./NavData";
 import Link from "next/link";
+import { BOTTOM_NAV_ICON_MAP } from "../svgs/bottom-nav/iconmap";
 
 export const BottomNav: React.FC = ({}) => {
   return (
     <ul className="py-5 w-full flex justify-around items-center fixed bottom-0 bg-inherit">
-      {NAV_LINKS.map(({ name, href }) => (
-        <Link passHref key={name} href={href}>
-          <li className="select-none cursor-pointer">
-            <img
-              className="w-8 h-8"
-              src={`/images/bottomnav/${name}.svg`}
-              alt=""
-            />
-          </li>
-        </Link>
-      ))}
+      {NAV_LINKS.map(({ name, href }) => {
+        const Icon = BOTTOM_NAV_ICON_MAP[name];
+
+        return (
+          <Link passHref key={name} href={href}>
+            <li className="select-none cursor-pointer">
+              {<Icon alt={name} className="w-8 h-8" />}
+            </li>
+          </Link>
+        );
+      })}
     </ul>
   );
 };
