001b28a5f906ecacdc8d58a4f078832190f0872b
Tim <harvard90873@gmail.com>
2022-05-22 16:22:09 +0200

    improve hook

--- src/utils/hooks.tsx
+++ src/utils/hooks.tsx
@@ -38,6 +38,7 @@ export const useOnScreen = <T extends HTMLElement | null>(
 export const useViewportClassname = <T extends HTMLElement>(
   ref: MutableRefObject<T | null>,
   viewportClassname = "",
+  nonViewportClassname = "",
   threshold = 0.05
 ) => {
   const isOnScreen = useOnScreen(ref, threshold);
@@ -47,11 +48,17 @@ export const useViewportClassname = <T extends HTMLElement>(
     if (!currNode) return;
 
     if (isOnScreen) {
-      currNode.className = currNode.className.concat(" " + viewportClassname);
+      currNode.className = currNode.className.concat(" ", viewportClassname);
     } else {
       if (currNode.className.includes(viewportClassname)) {
         currNode.className = currNode.className.replace(viewportClassname, "");
       }
+      if (!currNode.className.includes(nonViewportClassname)) {
+        currNode.className = currNode.className.concat(
+          " ",
+          nonViewportClassname
+        );
+      }
     }
-  }, [isOnScreen, ref, viewportClassname]);
+  }, [isOnScreen, ref, viewportClassname, nonViewportClassname]);
 };
