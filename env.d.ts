4ee1fb05f11ea5066e7ca5bdc5c111ad133e8cd9
Tim <65774333+timthedev07@users.noreply.github.com>
2022-05-28 18:19:02 +0000

    auth check on page

--- env.d.ts
+++ env.d.ts
@@ -6,6 +6,7 @@ declare global {
       DB_CONN_STRING: string;
       DB_NAME: string;
       DB_COLLECTION_NAME: string;
+      WHITELISTED_ADMIN_EMAILS: string;
       GOOGLE_CLIENT_ID: string;
       GOOGLE_CLIENT_SECRET: string;
       SPOTIFY_CLIENT_ID: string;
@@ -13,7 +14,6 @@ declare global {
       NEXT_AUTH_SECRET: string;
       DISCORD_CLIENT_ID: string;
       DISCORD_CLIENT_SECRET: string;
-      GITHUB_REST_TOKEN: string;
     }
   }
 }
---
+++ src/pages/blog/admin.tsx
@@ -0,0 +1,39 @@
+import { GetServerSideProps, NextPage } from "next";
+import { Session } from "next-auth";
+import { getSession } from "next-auth/react";
+
+interface Props {
+  session: Session & {
+    user: NonNullable<Session["user"]>;
+  };
+}
+
+const BlogAdmin: NextPage<Props> = ({ session }) => {
+  const { user } = session;
+
+  return <div>manage shit</div>;
+};
+
+export const isAdminEmail = (email: string) => {
+  return process.env.WHITELISTED_ADMIN_EMAILS.indexOf(email) > -1;
+};
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  req,
+}) => {
+  const session = await getSession({ req });
+
+  if (!session || !session.user?.email || isAdminEmail(session.user.email)) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      session: session as Props["session"],
+    },
+  };
+};
+
+export default BlogAdmin;
