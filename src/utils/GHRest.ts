06a9f8cbd79c148242fc64bd4502e1b527dba31c
Tim <harvard90873@gmail.com>
2022-05-29 00:14:51 +0200

    helpers for fetching stuff form github

---
+++ src/utils/GHRest.ts
@@ -0,0 +1,35 @@
+import { Octokit } from "octokit";
+
+export const octokit = new Octokit({
+  auth: process.env.GITHUB_REST_TOKEN,
+});
+
+export const getCategories = async (): Promise<string[]> => {
+  const a = await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+    owner: "timthedev07",
+    repo: "my-website",
+    path: "posts",
+  });
+  return (a.data as any[]).map((each) => each.name);
+};
+
+export const getCategoryPostNames = async (category: string) => {
+  const a = await octokit.request("GET /repos/{owner}/{repo}/contents/{path}", {
+    owner: "timthedev07",
+    repo: "my-website",
+    path: `posts/${category}`,
+  });
+  return (a.data as any[]).map((each) => each.name);
+};
+
+/**
+ *
+ * @param path  e.g. math/some-blog.md, school/sth.md
+ */
+export const readRemoteBlog = async (path: string) => {
+  const response = await fetch(
+    `https://raw.githubusercontent.com/timthedev07/my-website/dev/posts/${path}`
+  );
+
+  return await response.text();
+};
