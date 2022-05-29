ca54f452d08b8e2d360c59b0da4f079f350d636b
Tim <harvard90873@gmail.com>
2022-05-29 12:18:50 +0200

    add new api for fetching view count

---
+++ src/pages/api/viewcount/[blogId].ts
@@ -0,0 +1,29 @@
+import { NextApiHandler } from "next";
+import { collections, connectDB } from "../../../mongodb";
+
+const handler: NextApiHandler = async (req, res) => {
+  await connectDB();
+
+  const id = req.query.blogId;
+
+  if (!collections.blogViewCounts) {
+    res.status(503).end();
+    return;
+  }
+
+  try {
+    const dbRes = await collections.blogViewCounts.findOne({
+      blogId: id,
+    });
+    if (dbRes) {
+      res.status(200).send(dbRes.count);
+      res.end();
+    } else {
+      res.status(503).end();
+    }
+  } catch (err) {
+    res.status(503).end();
+  }
+};
+
+export default handler;
