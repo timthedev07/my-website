3be2f1648d0e353621cdcc558ab58f175be70913
Tim <harvard90873@gmail.com>
2022-05-29 17:10:04 +0200

    building editor

--- src/pages/blog/admin/[category]/[slug].tsx
+++ src/pages/blog/admin/[category]/[slug].tsx
@@ -1,5 +1,5 @@
 import type { GetServerSideProps, NextPage } from "next";
-import matter from "gray-matter";
+import matter, { stringify } from "gray-matter";
 import { MarkdownMetadata } from "../../../../types/posts";
 import { getHeadForPage } from "../../../../utils/getHead";
 import { readRemoteBlog } from "../../../../utils/GHRest";
@@ -7,7 +7,14 @@ import markdownToHtml from "../../../../utils/markdown";
 import RichMarkdownEditor from "@davidilie/markdown-editor";
 import { useNavContext } from "../../../../components/nav/Navbar";
 import { useEffect, useRef, useState } from "react";
-import { Button } from "dragontail-experimental";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+} from "dragontail-experimental";
+import { Drawer } from "dragontail-experimental";
 
 interface Props {
   content: string;
@@ -28,6 +35,7 @@ const Slug: NextPage<Props> = ({
     null
   );
   const editorRef = useRef<RichMarkdownEditor | null>(null);
+  const [newMetadata, setNewMetadata] = useState<MarkdownMetadata>(metadata);
 
   useEffect(() => {
     setNavTransparent(false);
@@ -54,11 +62,61 @@ const Slug: NextPage<Props> = ({
         />
       </div>
       <aside
-        className={`sticky rounded-md ${hackyHeight} w-72 border-l border-slate-700/60 bg-slate-800 flex flex-col items-center`}
+        className={`sticky rounded-md ${hackyHeight} py-4 w-96 border-l border-slate-700/60 bg-slate-800 flex flex-col items-center`}
       >
+        <h3 className="w-full text-left p-3 border-b-2 border-slate-500/50">
+          Edit
+        </h3>
+
+        <Drawer
+          className="w-full"
+          content={
+            <>
+              <FormControl label="keywords" isRequired>
+                <FormLabel>Comma separated</FormLabel>
+                <Input
+                  defaultValue={metadata.keywords.join(", ")}
+                  onChange={(e) => {
+                    const val = e.target.value;
+                    setNewMetadata((prev) => {
+                      return {
+                        ...prev,
+                        keywords: val.split(",").map((each) => each.trim()),
+                      };
+                    });
+                  }}
+                  value={newMetadata.keywords.join(", ")}
+                />
+              </FormControl>
+            </>
+          }
+          drawerLabel="Keywords"
+        />
+        <Drawer
+          className="w-full"
+          content={
+            <>
+              <FormControl label="description" isRequired>
+                <FormLabel>Blog summary</FormLabel>
+                <Textarea
+                  className="min-h-[130px]"
+                  defaultValue={metadata.description}
+                  value={newMetadata.description}
+                  onChange={(e) => {
+                    setNewMetadata((prev) => {
+                      return { ...prev, description: e.target.value };
+                    });
+                  }}
+                />
+              </FormControl>
+            </>
+          }
+          drawerLabel="Description"
+        />
+
         <Button
           color="green"
-          className="w-[90%]"
+          className="w-[90%] mt-auto"
           onClick={() => {
             const curr = editorRef.current;
             if (!curr) return;
@@ -66,6 +124,12 @@ const Slug: NextPage<Props> = ({
             const newVal = curr.value();
             console.log(newVal);
             setUpdatedFileContent(newVal);
+
+            // TO IMPLEMENT: upload to github
+            const encodedMeta = stringify("", newMetadata);
+            console.log(encodedMeta);
+
+            const final = encodedMeta + "\n" + newVal;
           }}
         >
           Save
