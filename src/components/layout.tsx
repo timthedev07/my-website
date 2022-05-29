1feea12b3514ab92c3780345614b5ea7c1d30811
Tim <harvard90873@gmail.com>
2022-05-29 15:35:14 +0200

    fix annoying shit

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -44,10 +44,10 @@ export const Layout: FC<{ children: React.ReactNode }> = ({ children }) => {
         <link rel="apple-touch-icon" href="/logo192.png" />
       </Head>
 
-      <div id="App" className={`bg-slate-900 min-h-screen`}>
+      <div id="App" className={`bg-slate-900`}>
         <AppLoadingProvider>
           <NavProvider>
-            <main className={`w-full min-h-screen text-white min-w-[250px]`}>
+            <main className={`w-full text-white min-w-[250px]`}>
               {children}
             </main>
           </NavProvider>
