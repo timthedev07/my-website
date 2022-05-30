1d5bbe5d58b7b7cad6ed68ca8c9f2ef627c7cf95
Tim <harvard90873@gmail.com>
2022-05-30 18:44:54 +0200

    add option to check for admin

--- src/lib/middlewares/authRoute.ts
+++ src/lib/middlewares/authRoute.ts
@@ -1,12 +1,21 @@
 import { NextApiHandler } from "next";
 import { getSession } from "next-auth/react";
+import { isAdminEmail } from "../../pages/blog/admin";
 
-export const withAuth = (handler: NextApiHandler): NextApiHandler => {
+export const withAuth = (
+  handler: NextApiHandler,
+  admin = false
+): NextApiHandler => {
   return async (req, res) => {
     const session = await getSession({ req });
 
     if (!session?.user) {
       res.status(401).send("Please sign in to continue");
+      return;
+    }
+
+    if (admin && (!session.user.email || !isAdminEmail(session.user.email))) {
+      res.status(401).send("You don't have permission to access this endpoint");
       return;
     }
 
