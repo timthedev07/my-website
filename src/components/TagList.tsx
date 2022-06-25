a07f201a6051a3e2d9b1f911868416855b9d0cc6
Tim <harvard90873@gmail.com>
2022-06-25 23:14:44 +0200

    create new component

---
+++ src/components/TagList.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react";
+
+interface TagListProps {
+  tagContents: string[];
+  className?: string;
+}
+
+const tagColors = [
+  "bg-yellow-600",
+  "bg-emerald-600",
+  "bg-violet-600",
+  "bg-cyan-600",
+  "bg-orange-600",
+  "bg-rose-600",
+  "bg-slate-500",
+];
+
+export const TagList: FC<TagListProps> = ({ tagContents, className = "" }) => {
+  return (
+    <ul className={`flex gap-3 ${className}`}>
+      {tagContents.map((tag, ind) => (
+        <li
+          key={tag}
+          className={`whitespace-nowrap font-semibold text-sm w-max rounded-md px-2 py-1 ${tagColors[ind]}`}
+        >
+          {tag}
+        </li>
+      ))}
+    </ul>
+  );
+};
