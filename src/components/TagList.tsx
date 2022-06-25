cb151ad25211a025952a2be7cc1afd68dc176ada
Tim <harvard90873@gmail.com>
2022-06-26 00:39:12 +0200

    randomize colors

--- src/components/TagList.tsx
+++ src/components/TagList.tsx
@@ -15,13 +15,25 @@ const tagColors = [
   "bg-slate-500",
 ];
 
+const shuffleArray = <T extends any>(array: Array<T>) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
 export const TagList: FC<TagListProps> = ({ tagContents, className = "" }) => {
   return (
     <ul className={`flex gap-3 ${className}`}>
       {tagContents.map((tag, ind) => (
         <li
           key={tag}
-          className={`whitespace-nowrap font-semibold text-sm w-max rounded-md px-2 py-1 ${tagColors[ind]}`}
+          className={`whitespace-nowrap font-semibold text-sm w-max rounded-md px-2 py-1 ${
+            shuffleArray(tagColors)[ind]
+          }`}
         >
           {tag}
         </li>
