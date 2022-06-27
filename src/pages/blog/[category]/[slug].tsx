ce2c1d4be8c07805bb9d32d09447db8547d2891a
Tim <harvard90873@gmail.com>
2022-06-28 00:22:20 +0200

    add image

--- src/pages/blog/[category]/[slug].tsx
+++ src/pages/blog/[category]/[slug].tsx
@@ -80,6 +80,7 @@ const Slug: NextPage<Props> = ({ content, metadataAsString, slug }) => {
         description: metadata.description,
         path: `/blog/${metadata.category}/${slug}`,
         keywords: metadata.keywords,
+        image: `/thumbnails/${slug}.png`,
       })}
       <div className="flex flex-col justify-center items-center">
         <section
