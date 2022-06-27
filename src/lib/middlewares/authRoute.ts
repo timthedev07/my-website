2aa798b314924027846fe1d511189346e8316af5
Tim <harvard90873@gmail.com>
2022-06-27 23:53:43 +0200

    fix

--- src/lib/middlewares/authRoute.ts
+++ src/lib/middlewares/authRoute.ts
@@ -1,21 +1,12 @@
 import { NextApiHandler } from "next";
 import { getSession } from "next-auth/react";
-import { isAdminEmail } from "../../pages/blog/admin";
 
-export const withAuth = (
-  handler: NextApiHandler,
-  admin = false
-): NextApiHandler => {
+export const withAuth = (handler: NextApiHandler): NextApiHandler => {
   return async (req, res) => {
     const session = await getSession({ req });
 
     if (!session?.user) {
       res.status(401).send("Please sign in to continue");
-      return;
-    }
-
-    if (admin && (!session.user.email || !isAdminEmail(session.user.email))) {
-      res.status(401).send("You don't have permission to access this endpoint");
       return;
     }
 
