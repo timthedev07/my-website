6b99647e24cead269b445e5b24e03943cbb10685
Tim <harvard90873@gmail.com>
2022-07-30 18:14:24 +0200

    add plugin for table render

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -4,13 +4,14 @@ import remarkMath from "remark-math";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import { unified } from "unified";
-// import rehypePrism from "rehype-prism";
+import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
 
 export default async function markdownToHtml(markdown: string) {
   const resultHTML = await unified()
     .use(remarkParse)
     .use(remarkMath)
+    .use(remarkGfm)
     .use(remarkRehype)
     .use(rehypeKatex, { strict: true })
     .use(rehypeHighlight)
