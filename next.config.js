fb2ec82dcc7c2df05daff8b2b6b718a7ed247b8e
Tim <harvard90873@gmail.com>
2022-07-31 23:28:50 +0200

    update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: false,
+  images: {
+    domains: ["raw.githubusercontent.com"],
+  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
