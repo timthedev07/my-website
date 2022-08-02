24f42b36c1f3e3dd732f2c00d41fd1eef5b782f3
Tim <harvard90873@gmail.com>
2022-08-02 23:45:50 +0200

    case insensitive

--- src/utils/arrays.ts
+++ src/utils/arrays.ts
@@ -1,11 +1,16 @@
 export const anyElementContains = (
   items: string[],
-  searchSubstring: string
+  searchSubstring: string,
+  caseInsensitive = true
 ) => {
   let found = false;
 
   for (const item of items) {
-    if (item.includes(searchSubstring)) {
+    if (
+      caseInsensitive
+        ? item.toLowerCase().includes(searchSubstring.toLowerCase())
+        : item.includes(searchSubstring)
+    ) {
       found = true;
       break;
     }
