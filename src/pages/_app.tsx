a225addad2028d080ea8189f9e827916d9e421dc
Tim <harvard90873@gmail.com>
2022-09-18 13:57:35 +0200

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,14 @@ import type { AppProps } from "next/app";
 import { Layout } from "../components/layout";
 import { SessionProvider } from "next-auth/react";
 import "../styles/globals.css";
+import { Session } from "next-auth";
 
-const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+const App = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{
+  session: Session;
+}>) => {
   return (
     <SessionProvider session={session}>
       <DragontailProvider theme="dark">
