9ce4baf24629c9f3c2e97f959ada958c7ea12b1b
Tim <harvard90873@gmail.com>
2023-04-08 22:42:22 +0200

    404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,38 @@
-import type { NextPage } from "next";
-import { useEffect } from "react";
-import { useNavContext } from "../components/nav/Navbar";
+import { Button } from "dragontail-experimental";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
 
 const NotFound: NextPage = () => {
-  const { setNavTransparent } = useNavContext();
+  const router = useRouter();
 
-  useEffect(() => {
-    setNavTransparent(false);
-  }, [setNavTransparent]);
-
-  return <div>404</div>;
+  return (
+    <>
+      <div className="w-full h-[70vh] flex flex-col gap-10 justify-center items-center pt-24 text-center">
+        <h1 className="text-red-400 font-bold text-8xl">404</h1>
+        <h4 className="text-red-200/80 text-lg font-semibold">
+          Page not Found
+        </h4>
+        <div className="w-full flex justify-center items-center gap-10">
+          <Button
+            onClick={() => {
+              if (router) router.back();
+            }}
+            color="emerald"
+          >
+            Previous
+          </Button>
+          <Button
+            onClick={() => {
+              router.push("/");
+            }}
+            color="orange"
+          >
+            Home
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
