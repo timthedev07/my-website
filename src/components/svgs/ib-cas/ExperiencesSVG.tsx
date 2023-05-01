4eaa751e2ce7fe8aa73bea840b9578256dee79d5
Tim <harvard90873@gmail.com>
2023-05-02 00:21:41 +0200

    new svgs

---
+++ src/components/svgs/ib-cas/ExperiencesSVG.tsx
@@ -0,0 +1,203 @@
+import { forwardRef, SVGProps } from "react";
+
+export const ExperiencesSVG = forwardRef<
+  SVGSVGElement,
+  SVGProps<SVGSVGElement>
+>(({ ...props }, ref) => {
+  return (
+    <svg
+      ref={ref}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlSpace="preserve"
+      viewBox="0 0 64 64"
+      {...props}
+    >
+      <g transform="translate(0 -384)">
+        <path
+          d="M10 393h49v6H10z"
+          style={{
+            fill: "#3e4f59",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeWidth: 2,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M10 399h49v40H10z"
+          style={{
+            fill: "#acbec2",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeWidth: 2.00001,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M10 399v40h29.77a28.484 41.393 35.6 0 0 18.625-40z"
+          style={{
+            fill: "#e8edee",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            stroke: "none",
+            strokeWidth: 2.00002,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M12 392c-1.645 0-3 1.355-3 3v40a1 1 0 1 0 2 0v-40c0-.564.436-1 1-1h45c.564 0 1 .436 1 1v3H16a1 1 0 0 0 0 2h42v37c0 .564-.436 1-1 1H8a1 1 0 0 0 0 2h49c1.645 0 3-1.355 3-3v-42c0-1.645-1.355-3-3-3z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M55 395a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+          style={{
+            color: "#000",
+            fill: "#ed7161",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M51 395a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+          style={{
+            color: "#000",
+            fill: "#ecba16",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M47 395a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+          style={{
+            color: "#000",
+            fill: "#42b05c",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M5 438a1 1 0 0 0-1 1 1 1 0 0 0 1 1 1 1 0 0 0 1-1 1 1 0 0 0-1-1zM13 398a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <rect
+          width={32}
+          height={20}
+          x={18}
+          y={409}
+          rx={2}
+          ry={2}
+          style={{
+            fill: "#ffa221",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            stroke: "none",
+            strokeWidth: 1.99999,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M20.01 409.871a29.276 14.052 15 0 0-1.715.084c-.186.304-.295.66-.295 1.045v16c0 1.108.892 2 2 2h28c1.108 0 2-.892 2-2v-8.488a29.276 14.052 15 0 0-15.363-6.672 29.276 14.052 15 0 0-14.627-1.969z"
+          style={{
+            fill: "#ffc343",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeWidth: 2,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M39 412a1 1 0 0 0-1 1 1 1 0 0 0 1 1h7a1 1 0 0 0 1-1 1 1 0 0 0-1-1z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M35 412a1 1 0 1 0 0 2 1 1 0 0 0 0-2zM32 412a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M26 417h8v6h-8z"
+          style={{
+            fill: "#0588e2",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeWidth: 2,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M42 423h8v6h-8z"
+          style={{
+            fill: "#fe93d3",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeWidth: 2,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+        <path
+          d="M20 408c-1.645 0-3 1.355-3 3v16c0 1.645 1.355 3 3 3h6c.077 0 .127-.01.166-.027A1 1 0 0 0 27 429v-5h6v4h-3a1 1 0 1 0 0 2h18c1.645 0 3-1.355 3-3v-16c0-1.645-1.355-3-3-3h-2.01a1 1 0 1 0 0 2H48c.571 0 1 .429 1 1v5h-9a1 1 0 0 0 0 2h1v4h-6v-4h1a1 1 0 0 0 0-2H19v-5c0-.571.429-1 1-1h22.084a1 1 0 1 0 0-2zm-1 10h6v4h-6zm8 0h6v4h-6zm16 0h6v4h-6zm-24 6h6v4h-5c-.571 0-1-.429-1-1zm16 0h6v4h-6zm8 0h6v3c0 .571-.429 1-1 1h-5z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+        />
+      </g>
+    </svg>
+  );
+});
+
+ExperiencesSVG.displayName = "Experiences";
---
+++ src/components/svgs/ib-cas/ProjectsSVG.tsx
@@ -0,0 +1,237 @@
+import { forwardRef, SVGProps } from "react";
+
+export const ProjectsSVG = forwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>(
+  ({ ...props }, ref) => {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlSpace="preserve"
+        viewBox="0 0 64 64"
+        ref={ref}
+        {...props}
+      >
+        <path
+          d="M10 105h49v6H10Z"
+          style={{
+            fill: "#3e4f59",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeWidth: 2,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M10 111h49v40H10Z"
+          style={{
+            fill: "#acbec2",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeWidth: 2.00001,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M10 111v40h29.77a28.484 41.393 35.6 0 0 18.625-40z"
+          style={{
+            fill: "#e8edee",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            stroke: "none",
+            strokeWidth: 2.00002,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M12 104c-1.645 0-3 1.355-3 3v40a1 1 0 1 0 2 0v-40c0-.564.436-1 1-1h45c.564 0 1 .436 1 1v3H16a1 1 0 0 0 0 2h42v37c0 .564-.436 1-1 1H8a1 1 0 0 0 0 2h49c1.645 0 3-1.355 3-3v-42c0-1.645-1.355-3-3-3z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M55 107a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+          style={{
+            color: "#000",
+            fill: "#ed7161",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M51 107a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+          style={{
+            color: "#000",
+            fill: "#ecba16",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M47 107a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+          style={{
+            color: "#000",
+            fill: "#42b05c",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M5 150a1 1 0 0 0-1 1 1 1 0 0 0 1 1 1 1 0 0 0 1-1 1 1 0 0 0-1-1zM13 110a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M23 125h18v17H23a1 1 0 0 1-1-1v-15a1 1 0 0 1 1-1z"
+          style={{
+            fill: "#fe73c5",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            stroke: "none",
+            strokeWidth: 2,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M23 124c-1.093 0-2 .907-2 2v2a1 1 0 0 0 1 1 1 1 0 0 0 1-1v-2h17v15H23v-9a1 1 0 0 0-1-1 1 1 0 0 0-1 1v9c0 1.093.907 2 2 2h18a1 1 0 0 0 1-1v-17a1 1 0 0 0-1-1z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M30 120h11a1 1 0 0 1 1 1v11H29v-11a1 1 0 0 1 1-1z"
+          style={{
+            fill: "#0075d3",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            stroke: "none",
+            strokeWidth: 2,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M30 120a1 1 0 0 0-1 1v11h9.025a8.951 9.816 0 0 0 1.913-6.027 8.951 9.816 0 0 0-1.85-5.973Z"
+          style={{
+            fill: "#0588e2",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            stroke: "none",
+            strokeWidth: 2.00001,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M33.277 126a1 1 0 0 0-1 1 1 1 0 0 0 1 1h5.446a1 1 0 0 0 1-1 1 1 0 0 0-1-1z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -100)"
+        />
+        <path
+          d="M30 119c-1.093 0-2 .907-2 2v11a1 1 0 0 0 1 1h13a1 1 0 0 0 1-1v-4.99a1 1 0 0 0-2 0V131H30v-10h11v1.963a1 1 0 0 0 2 0V121c0-1.093-.907-2-2-2z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M33.277 129a1 1 0 0 0-1 1 1 1 0 0 0 1 1h5.446a1 1 0 0 0 1-1 1 1 0 0 0-1-1z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -100)"
+        />
+        <path
+          d="M28 129h18a.687.687 0 0 1 .641.933l-4.282 11.134A1.456 1.456 0 0 1 41 142H23a.687.687 0 0 1-.641-.933l4.282-11.134A1.456 1.456 0 0 1 28 129Z"
+          style={{
+            fill: "#fe93d3",
+            fillOpacity: 1,
+            fillRule: "evenodd",
+            stroke: "none",
+            strokeWidth: 2,
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+        <path
+          d="M28 128c-1.013 0-1.93.629-2.293 1.574l-4.281 11.133C21.013 141.781 21.849 143 23 143h18c1.013 0 1.93-.629 2.293-1.574l1.473-3.83a1 1 0 0 0-.575-1.293 1 1 0 0 0-1.293.574l-1.472 3.83A.45.45 0 0 1 41 141H23.457l4.117-10.707A.45.45 0 0 1 28 130h17.543l-1.258 3.273a1 1 0 0 0 .574 1.293 1 1 0 0 0 1.293-.574l1.422-3.699C47.987 129.219 47.151 128 46 128Z"
+          style={{
+            color: "#000",
+            fill: "#000",
+            fillRule: "evenodd",
+            strokeLinecap: "round",
+            strokeLinejoin: "round",
+            strokeMiterlimit: 4.1,
+          }}
+          transform="translate(0 -96)"
+        />
+      </svg>
+    );
+  }
+);
+
+ProjectsSVG.displayName = "Projects";
