14c927eef8dd09a275aacdf2cf0811371b75fbfe
timthedev07 <timpersonal07@gmail.com>
2023-08-02 22:50:15 +0200

    add function

--- src/lib/ib-cas/reflections-mdx.ts
+++ src/lib/ib-cas/reflections-mdx.ts
@@ -9,6 +9,10 @@ const CAS_ROOT = "ib-cas-mdx";
 
 const DATA_DIR = join(CAS_ROOT, "reflections");
 
+export const getYearAndMonth = (yearMonth: string) => {
+  return yearMonth.split("-");
+};
+
 export const getAllMonths = () => {
   return [
     ...new Set(
@@ -17,6 +21,17 @@ export const getAllMonths = () => {
   ]
     .sort()
     .reverse(); // sort descending, latest first
+};
+
+export const groupByYear = (yearMonths: string[]) => {
+  // this maps yyyy to yyyy-mm
+  const groups: Record<string, string[]> = {};
+  for (const yearMonth of yearMonths) {
+    const [year] = getYearAndMonth(yearMonth);
+    groups[year].push(yearMonth);
+  }
+
+  return groups;
 };
 
 export const getMonthEntries = async (month: string) => {
