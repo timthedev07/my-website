4bfddab1a1ac110361e647f45f05761e8fb337ca
timthedev07 <timpersonal07@gmail.com>
2023-08-04 17:23:55 +0200

    new pages

---
+++ src/pages/ib/cas/experiences/[date].tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const ExperienceEntryPage: NextPage = () => {
+  return <></>;
+};
+
+export default ExperienceEntryPage;
---
+++ src/pages/ib/cas/experiences/index.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+
+// timeline style page
+const ExperiencesIndex: NextPage = () => {
+  return <></>;
+};
+
+export default ExperiencesIndex;
