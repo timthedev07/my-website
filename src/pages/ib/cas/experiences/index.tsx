52fc83d0cfdf68ce9d341e7d441bc626e541a075
timthedev07 <timpersonal07@gmail.com>
2023-08-04 18:19:29 +0200

    add bg for experiences index

--- src/pages/ib/cas/experiences/index.tsx
+++ src/pages/ib/cas/experiences/index.tsx
@@ -1,8 +1,12 @@
 import { NextPage } from "next";
 
-// timeline style page
+// timeline style page; from newest to oldest
 const ExperiencesIndex: NextPage = () => {
-  return <></>;
+  return (
+    <div className="w-screen h-screen">
+      <div className="w-full h-full fixed" id="experience-index"></div>
+    </div>
+  );
 };
 
 export default ExperiencesIndex;
