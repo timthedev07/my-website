d90d300d182a07ea45c667a64ed884df18d3c630
timthedev07 <timpersonal07@gmail.com>
2023-08-06 16:21:15 +0200

    add new bg

---
+++ src/components/svgs/ib-cas/ExperiencesBG.tsx
@@ -0,0 +1,167 @@
+import { forwardRef, SVGProps } from "react";
+
+export const ExperiencesBGSVG = forwardRef<
+  SVGSVGElement,
+  SVGProps<SVGSVGElement>
+>(({ ...props }, ref) => {
+  return (
+    <svg ref={ref} viewBox="0 0 1200 1200" {...props}>
+      <g strokeLinejoin="bevel">
+        <path fill="#205a7a" stroke="#205a7a" d="m604 575 198-41-172-93Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="m802 534 4-13-176-80Z" />
+        <path fill="#1d4e6b" stroke="#1d4e6b" d="m806 521-37-173-139 93Z" />
+        <path fill="#15384f" stroke="#15384f" d="m769 348-99-17-40 110Z" />
+        <path fill="#081a28" stroke="#081a28" d="m604 575 136 155 62-196Z" />
+        <path fill="#19435d" stroke="#19435d" d="m1002 460-90-62-106 123Z" />
+        <path fill="#081a28" stroke="#081a28" d="m806 521 106-123-143-50Z" />
+        <path fill="#19435d" stroke="#19435d" d="M962 645 806 521l-4 13Z" />
+        <path
+          fill="#297ea6"
+          stroke="#297ea6"
+          d="m369 521 121 143 114-89ZM604 575l-73 228 209-73Z"
+        />
+        <path
+          fill="#081a28"
+          stroke="#081a28"
+          d="m413 404 191 171 26-134ZM740 730l162-2-100-194Z"
+        />
+        <path fill="#19435d" stroke="#19435d" d="m670 331-220 38 180 72Z" />
+        <path fill="#246688" stroke="#246688" d="m902 728 60-83-160-111Z" />
+        <path fill="#1d4e6b" stroke="#1d4e6b" d="m912 398-68-144-75 94Z" />
+        <path
+          fill="#246688"
+          stroke="#246688"
+          d="m769 348 41-170-140 153ZM670 331 477 217l-27 152Z"
+        />
+        <path fill="#297ea6" stroke="#297ea6" d="m450 369-37 35 217 37Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="m962 645 40-185-196 61Z" />
+        <path fill="#00101c" stroke="#00101c" d="m1110 197-236-45-30 102Z" />
+        <path fill="#19435d" stroke="#19435d" d="m391 735 140 68-41-139Z" />
+        <path fill="#246688" stroke="#246688" d="m490 664 41 139 73-228Z" />
+        <path fill="#297ea6" stroke="#297ea6" d="m740 730 81 117 81-119Z" />
+        <path fill="#19435d" stroke="#19435d" d="m413 404-44 117 235 54Z" />
+        <path fill="#277297" stroke="#277297" d="m531 803 85 43 124-116Z" />
+        <path fill="#246688" stroke="#246688" d="m616 846 205 1-81-117Z" />
+        <path
+          fill="#297ea6"
+          stroke="#297ea6"
+          d="m1061 748 57-155-156 52ZM874 152l-64 26 34 76Z"
+        />
+        <path fill="#297ea6" stroke="#297ea6" d="m844 254-34-76-41 170Z" />
+        <path
+          fill="#277297"
+          stroke="#277297"
+          d="m307 602 84 133 99-71ZM531 803 410 903l206-57Z"
+        />
+        <path fill="#081a28" stroke="#081a28" d="M612 103 477 217l193 114Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="m450 369-168-29 131 64Z" />
+        <path fill="#277297" stroke="#277297" d="m282 340 25 262 62-81Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="m791 1049 59-168-29-34Z" />
+        <path fill="#112e42" stroke="#112e42" d="m821 847 29 34 52-153Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="m369 521-62 81 183 62Z" />
+        <path fill="#081a28" stroke="#081a28" d="m282 340 87 181 44-117Z" />
+        <path fill="#19435d" stroke="#19435d" d="m1109 340-265-86 68 144Z" />
+        <path fill="#297ea6" stroke="#297ea6" d="m810 178-198-75 58 228Z" />
+        <path fill="#00101c" stroke="#00101c" d="m962 645 156-52-116-133Z" />
+        <path fill="#297ea6" stroke="#297ea6" d="m1002 460 107-120-197 58Z" />
+        <path fill="#15384f" stroke="#15384f" d="m1061 748-99-103-60 83Z" />
+        <path fill="#1d4e6b" stroke="#1d4e6b" d="m988 883 73-135-159-20Z" />
+        <path fill="#297ea6" stroke="#297ea6" d="m688 0-76 103 198 75Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="m1200 439-91-99-107 120Z" />
+        <path fill="#112e42" stroke="#112e42" d="m257 212 25 128 195-123Z" />
+        <path fill="#081a28" stroke="#081a28" d="M477 217 282 340l168 29Z" />
+        <path fill="#246688" stroke="#246688" d="m850 881 138 2-86-155Z" />
+        <path fill="#1d4e6b" stroke="#1d4e6b" d="m391 735 19 168 121-100Z" />
+        <path fill="#112e42" stroke="#112e42" d="m616 846 175 203 30-202Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="m850 881 17 169 121-167Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="m328 850 82 53-19-168Z" />
+        <path fill="#15384f" stroke="#15384f" d="m227 749 101 101 63-115Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="m227 749 164-14-84-133Z" />
+        <path fill="#277297" stroke="#277297" d="M1200 664V439l-82 154Z" />
+        <path fill="#297ea6" stroke="#297ea6" d="m1118 593 82-154-198 21Z" />
+        <path fill="#081a28" stroke="#081a28" d="m91 561 136 188 80-147Z" />
+        <path fill="#277297" stroke="#277297" d="m1061 748 139-84-82-71Z" />
+        <path fill="#19435d" stroke="#19435d" d="M1200 746v-82l-139 84Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="M943 0H688l186 152Z" />
+        <path fill="#112e42" stroke="#112e42" d="M874 152 688 0l122 178Z" />
+        <path fill="#19435d" stroke="#19435d" d="M612 103 446 0l31 217Z" />
+        <path fill="#00101c" stroke="#00101c" d="m1200 308-90-111-1 143Z" />
+        <path fill="#112e42" stroke="#112e42" d="m1109 340 1-143-266 57Z" />
+        <path fill="#00101c" stroke="#00101c" d="m307 1076 116-99-95-127Z" />
+        <path fill="#19435d" stroke="#19435d" d="m328 850 95 127-13-74Z" />
+        <path fill="#15384f" stroke="#15384f" d="m410 903 13 74 193-131Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="m561 1115 230-66-175-203Z" />
+        <path fill="#081a28" stroke="#081a28" d="M688 0H587l25 103Z" />
+        <path fill="#00101c" stroke="#00101c" d="M364 0 257 212l220 5Z" />
+        <path fill="#246688" stroke="#246688" d="m0 663 209 122 18-36Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="M1200 439V308l-91 32Z" />
+        <path fill="#246688" stroke="#246688" d="m1200 845-139-97-73 135Z" />
+        <path fill="#277297" stroke="#277297" d="M1200 845v-99l-139 2Z" />
+        <path fill="#297ea6" stroke="#297ea6" d="m91 561 216 41-25-262Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="m227 749-18 36 119 65Z" />
+        <path fill="#246688" stroke="#246688" d="m791 1049 76 1-17-169Z" />
+        <path fill="#15384f" stroke="#15384f" d="M1019 0h-76l-69 152Z" />
+        <path fill="#00101c" stroke="#00101c" d="M587 0H446l166 103Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="m0 803 203 27 6-45Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="m209 785-6 45 125 20Z" />
+        <path fill="#00101c" stroke="#00101c" d="m184 0 12 202 61 10Z" />
+        <path fill="#277297" stroke="#277297" d="m257 212-101 38 126 90Z" />
+        <path fill="#00101c" stroke="#00101c" d="m1112 1078 88-233-212 38Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="m196 202-40 48 101-38Z" />
+        <path fill="#297ea6" stroke="#297ea6" d="m90 380 1 181 191-221Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="m483 1200 78-85-138-138Z" />
+        <path fill="#15384f" stroke="#15384f" d="m423 977 138 138 55-269Z" />
+        <path fill="#277297" stroke="#277297" d="m791 1049 28 151 48-150Z" />
+        <path fill="#297ea6" stroke="#297ea6" d="M1200 106 1019 0l91 197Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="M1110 197 1019 0 874 152Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="M446 0h-82l113 217Z" />
+        <path fill="#246688" stroke="#246688" d="M0 172v195l156-117Z" />
+        <path fill="#19435d" stroke="#19435d" d="M156 250 90 380l192-40Z" />
+        <path fill="#1d4e6b" stroke="#1d4e6b" d="M1200 308V106l-90 91Z" />
+        <path fill="#297ea6" stroke="#297ea6" d="m203 830 104 246 21-226Z" />
+        <path fill="#15384f" stroke="#15384f" d="m561 1115 258 85-28-151Z" />
+        <path fill="#297ea6" stroke="#297ea6" d="m867 1050 245 28-124-195Z" />
+        <path fill="#277297" stroke="#277297" d="M559 1200h260l-258-85Z" />
+        <path fill="#1d4e6b" stroke="#1d4e6b" d="M483 1200h76l2-85Z" />
+        <path fill="#15384f" stroke="#15384f" d="m1004 1200 108-122-245-28Z" />
+        <path fill="#297ea6" stroke="#297ea6" d="M0 367v149l90-136Z" />
+        <path fill="#246688" stroke="#246688" d="M90 380 0 516l91 45Z" />
+        <path fill="#19435d" stroke="#19435d" d="M91 561 0 663l227 86Z" />
+        <path
+          fill="#081a28"
+          stroke="#081a28"
+          d="m203 830-16 274 120-28ZM307 1076l176 124-60-223Z"
+        />
+        <path fill="#1d4e6b" stroke="#1d4e6b" d="M0 516v147l91-102Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="M156 250 0 367l90 13Z" />
+        <path fill="#246688" stroke="#246688" d="m0 172 156 78 40-48Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="M1200 106V0h-181Z" />
+        <path fill="#00101c" stroke="#00101c" d="M819 1200h144l-96-150Z" />
+        <path
+          fill="#081a28"
+          stroke="#081a28"
+          d="M364 0H184l73 212ZM963 1200h41l-137-150Z"
+        />
+        <path fill="#0c2435" stroke="#0c2435" d="M0 663v140l209-18Z" />
+        <path fill="#081a28" stroke="#081a28" d="M1200 1200v-106l-88-16Z" />
+        <path fill="#112e42" stroke="#112e42" d="m1112 1078 88 16V845Z" />
+        <path fill="#15384f" stroke="#15384f" d="m187 1104 139 96-19-124Z" />
+        <path fill="#1d4e6b" stroke="#1d4e6b" d="m307 1076 19 124h157Z" />
+        <path fill="#0c2435" stroke="#0c2435" d="m0 861 187 243 16-274Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="M0 803v58l203-31Z" />
+        <path
+          fill="#246688"
+          stroke="#246688"
+          d="M184 0 0 172l196 30ZM1004 1200h196l-88-122Z"
+        />
+        <path fill="#00101c" stroke="#00101c" d="M184 0H0v172Z" />
+        <path fill="#081a28" stroke="#081a28" d="m0 1104 124 96 63-96Z" />
+        <path fill="#246688" stroke="#246688" d="m187 1104-63 96h202Z" />
+        <path fill="#205a7a" stroke="#205a7a" d="M0 861v243h187Z" />
+        <path fill="#19435d" stroke="#19435d" d="M0 1104v96h124Z" />
+      </g>
+    </svg>
+  );
+});
+
+ExperiencesBGSVG.displayName = "ExperiencesBG";
