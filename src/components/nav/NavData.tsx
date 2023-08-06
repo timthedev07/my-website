cb90c4795d5bc0ed7c642a05b7931332fd0196bf
timthedev07 <timpersonal07@gmail.com>
2023-08-06 16:29:07 +0200

    fix

--- src/components/nav/NavData.tsx
+++ src/components/nav/NavData.tsx
@@ -8,17 +8,17 @@ export const NAV_LINKS: NavbarItemProps[] = [
     href: "/",
     name: "home",
   },
-  {
-    href: "/projects",
-    name: "projects",
-  },
+  // {
+  //   href: "/projects",
+  //   name: "projects",
+  // },
   {
     href: "/blog",
     name: "blog",
   },
   {
     href: "/ib/cas",
-    name: "ib-cas"
+    name: "ib-cas",
   },
   {
     href: "/contact",
