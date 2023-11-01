50c2accb8f0a105b1b047c90b1b460bf47e1943b
timthedev07 <timpersonal07@gmail.com>
2023-11-01 21:20:39 +0100

    new achievement lol

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -167,7 +167,10 @@ const Home: NextPage = () => {
                 Gold in UKMT Intermediate Maths Challenge for 2 consecutive
                 years; Best of School in 2023
               </li>
-              <li>Gold in UKMT Senior Maths Challenge, 2023</li>
+              <li>
+                Gold, best of year and second of school in UKMT Senior Maths
+                Challenge as a Y12, 2023
+              </li>
               <li>Merit, UKMT Maclaurin 2023</li>
               <li>
                 Province representative in &quot;Fase Autonòmica Olimpíada
