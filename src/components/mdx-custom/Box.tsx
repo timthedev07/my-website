f8cabb529aa970986eba9cfff5df27aa782f0982
timthedev07 <timpersonal07@gmail.com>
2023-11-13 20:15:30 +0100

    complete first

--- src/components/mdx-custom/Box.tsx
+++ src/components/mdx-custom/Box.tsx
@@ -8,11 +8,11 @@ interface BoxProps {
 }
 
 const colors: Record<BoxColorScheme, [string, string]> = {
-  blue: ["bg-cyan-500/70", "border-cyan-400/50"],
-  dark: ["bg-slate-500/70", "border-slate-400/50"],
-  green: ["bg-emerald-500/70", "border-emerald-400/50"],
-  orange: ["bg-orange-500/70", "border-orange-400/50"],
-  red: ["bg-red-500/70", "border-red-400/50"],
+  blue: ["bg-cyan-500/80", "border-cyan-400/50"],
+  dark: ["bg-slate-500/80", "border-slate-400/50"],
+  green: ["bg-emerald-500/80", "border-emerald-400/50"],
+  orange: ["bg-orange-500/80", "border-orange-400/50"],
+  red: ["bg-red-500/80", "border-red-400/50"],
 };
 
 export const Box: FC<BoxProps> = ({ color, children }) => {
@@ -20,7 +20,7 @@ export const Box: FC<BoxProps> = ({ color, children }) => {
     <div
       className={`rounded-md my-6 border-2 ${colors[color].join(
         " "
-      )} flex px-8 py-5`}
+      )} flex flex-col px-8 py-5`}
     >
       {children}
     </div>
